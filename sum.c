4ebc183807bc48fa0cfa176a0e150daab03403f5
jungprajwol <prajwolthapa.080@kathford.edu.np>
2024-07-07 15:18:02 +0545

    Code push

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main ()
+{
+   int a,b,sum;
+   printf("Enter the first number");
+    scanf("%d",&a); 
+    printf("Enter the second number");
+     scanf("%d" ,&b);
+    printf("the sum is " ,a+b);
+    }
